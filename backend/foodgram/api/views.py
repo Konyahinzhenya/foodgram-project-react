fef43df85408c55fe66225e1759464a4f9bab1d6
Evgeniy konyakhin <konyahinzhenya@yandex.ru>
2023-05-30 19:06:32 +0300

    fix model

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
 from foodgram.settings import FILE_NAME
 from recipes.models import (Favorite, Ingredient, Recipe, RecipeIngredient,
-                            Shopping_cart, Tag)
+                            ShoppingCart, Tag)
 from rest_framework import filters, mixins, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
@@ -151,16 +151,16 @@ class RecipeViewSet(viewsets.ModelViewSet):
             serializer = RecipeSerializer(recipe, data=request.data,
                                           context={"request": request})
             serializer.is_valid(raise_exception=True)
-            if not Shopping_cart.objects.filter(user=request.user,
+            if not ShoppingCart.objects.filter(user=request.user,
                                                 recipe=recipe).exists():
-                Shopping_cart.objects.create(user=request.user, recipe=recipe)
+                ShoppingCart.objects.create(user=request.user, recipe=recipe)
                 return Response(serializer.data,
                                 status=status.HTTP_201_CREATED)
             return Response({'errors': 'Рецепт уже в списке покупок.'},
                             status=status.HTTP_400_BAD_REQUEST)
 
         if request.method == 'DELETE':
-            get_object_or_404(Shopping_cart, user=request.user,
+            get_object_or_404(ShoppingCart, user=request.user,
                               recipe=recipe).delete()
             return Response(
                 {'detail': 'Рецепт успешно удален из списка покупок.'},
